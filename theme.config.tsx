44790b59722cedfa62fd48a6e64686d78f7a2d02
PwnOwn <aloutsss@outlook.com>
2024-12-16 13:44:14 +0800

    lol

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/PwnOwn/documentation-starter-kit',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/PwnOwn/documentation-starter-kit',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Docs',
   },
 }
 
